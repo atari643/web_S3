bf6e962afa67c74435a5b747fa1965fb0fd79a7a
qartigala <qartigala@etu.iut-bordeaux.fr>
2023-12-12 14:05:56 +0100

    Fix redirection issue in verify.php

--- TD5/verify.php
+++ TD5/verify.php
@@ -13,7 +13,8 @@ if($result){
     if($_SESSION['HTTP_REFERER'] == NULL){
         $_SESSION['HTTP_REFERER']=$_SERVER['HTTP_REFERER'];
     }
-    if($_SESSION['HTTP_REFERER'] != 'http://localhost:8800/TD5/connexion.php' && $_SESSION['HTTP_REFERER'] != 'http://localhost:8800/TD5/deconnexion.php'){
+    if($_SESSION['HTTP_REFERER'] != 'http://localhost:8800/TD5/connexion.php' && $_SESSION['HTTP_REFERER'] != 'http://localhost:8800/TD5/deconnexion.php' &&
+        $_SESSION['HTTP_REFERER'] != 'http://localhost:8800/TD2/accueil.php'){
         header("Location: " . $_SESSION['HTTP_REFERER']);
     }else if($_SESSION['HTTP_REFERER'] == 'http://localhost:8800/TD5/verify.php'){
         header('Location: ../TD3&TD4/formulaireBD.php');
